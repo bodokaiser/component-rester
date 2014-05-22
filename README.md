224dc4a4b5e0638abd7916ddd0d80f7a4f3e074b
Bodo Kaiser <i@bodokaiser.io>
2014-05-22 10:47:40 +0200

    rewritting API to be more like superagent

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # component-rester
 
-Your sweet REST client based on superagent.
+Your sweet REST client based on
+[superagent](https://github.com/visionmedia/superagent).
 
 ## Usage
 
@@ -11,18 +12,24 @@ Your sweet REST client based on superagent.
     client.on('error', function(error) {
       // handle error in application or view
     });
-    client.on('find', function(ok, body) {
-      // render models in view
+    client.on('response', function(response) {
+      // handle 4xx and 5xx statuses via view
     });
-    // will do a GET /products?filter[name]=Bag
-    client.find({ filter: { name: 'Bag' } });
 
-    // or directly with callback
-    client.find({ filter: { name: 'Bag' } }, function(err, res) {
-      if (err) throw err;
+    // will do a GET /products?filter[name]=Bag and return a request object
+    client.find({ filter: { name: 'Bag' } })
+      .accept('json')
+      .end(function(res) {
+        // handle response
+      });
 
-      // else handle response
+    // will do a GET /products and directly assign the callback to .end()
+    client.find(function(res) {
+      // handle response
     });
+
+    // will do a PUT /products/123
+    client.persist({ id: 123, name: 'book' });
 
 
 ## Install
@@ -37,20 +44,27 @@ or with [npm](https://github.com/npm/npm)
 
 ## Documentation
 
-The exposed function behind `rester` is a factory which will instance the
-`Rester` prototype for you. The `Rester` prototype does nothing more then
-to add some REST API sugar on superagent. Also by using the reset function
-to create instances of `Rester` you can centralize error management and setup
-plugins.
+The `Rester` prototype is an instance of `component-emitter` with some
+additional API sugar for HTTP rest on superagent.
 
-### rester(url[, options])
+### rester(url)
 
 Will return an instance of `Rester` which will be bound to specified `url`.
 This means that all requests will be prefixed to the `url`.
 
-The `options` parameter is currently undefined. We may use it in future.
+### rester.use(plugin)
 
-### Event: "error"
+rester.use(function(request) {
+  request.set('Authorization', 'Bearer ' + sessionStorage.get('token'));
+});
+
+Will register a plugin on each `Rester` instance. A plugin basically appends
+logic to the superagents `request` before `request.end` is called. You can
+use it for authorization (e.g. JSON-Web-Tokens).
+
+### Class: Rester
+
+#### Event: "error"
 
     rester.on('error', function(error) {
       // lets do it the easy way
@@ -59,93 +73,88 @@ The `options` parameter is currently undefined. We may use it in future.
 
 Will be emitted each time a superagent error occurs (e.g. "network error").
 
-Intended for centralized error management. For example you could shutdown your
-application on an error or render an error message through notifications
-however you are not forced to do this out of each single request.
+Intended for centralized error management. For example you could render an
+error message through notifications however you are not forced to do this out
+of each single request.
 
-### Event: "status"
+#### Event: "response"
 
-    rester.on('status', function(status, body, res) {
-      if (status === 400) return view.notify('There was an error in your data.');
+    rester.on('response', function(response) {
+      if (!response.ok) return view.notify('This was not okay.');
     });
 
-Will be emitted for special status handling.
+Will be emitted on each successful response. Though you could use this to
+omit traditional callbacks it is more likely to use the `response` event to
+handle non-successful status codes.
 
-As for example a `bad request` response is not an error per definition (mostly
-it is a validation error) superagent handles them as success. However you
-may want to bind to specific 4xx statuses by subscribing to this event.
-
-### rester.use(plugin)
-
-    rester.use(function(request) {
-      request.set('Authorization', 'Bearer ' + sesstionStorage.get('token'));
-    });
-
-Will register a plugin on each `Rester` instance. A plugin basically appends
-logic to the superagents `request` before `request.end` is called. You can
-use it for authentorization (e.g. JSON-Web-Tokens) or more.
-
-#### client
-
-Following methods are only available on direct `Rester` instances not on the
-global namespace.
-
-#### client.find([query, callback])
+#### rester.find([query, callback])
 
     // does a GET /<endpoint>
-    client.find(function(ok, body, response) {
-      if (!ok) return;
-
-      // use body
+    client.find(function(response) {
+      // do something with superagents response object
     });
 
     // does a GET /<endpoint>?limit=10
-    client.find({ limit: 10 }, function(ok, body, response) {
-      if (!ok) return;
-
-      // use body
+    client.find({ limit: 10 }, function(response) {
+      // do something with superagents response object
     });
+
+    client.find({ limit: 5 }, function(error, response) {
+      // as we just decorate superagent you can copy its API.
+      // providing two arguments will give you the error object
+    });
+
+    // or append logic on the returned request object
+    client.find()
+      .auth('foo', 'bar')
+      .accept('json')
+      .end(function(response) {
+        // handle response
+      });
+
 
 Will do a `GET` request against the specified endpoint where optional `query`
 is used as query string parameter and the optional `callback` executed on
-success (non-error) else it will emit a `find` event.
+success (non-error). If no callback is provided it will return a `request`.
 
 #### client.findOne(query[, callback])
 
     // does a GET /<endpoint>/123?include=true
-    client.findOne({ id: 123, include: true });
-
-    // does a GET /<endpoint>/123
-    client.findOne(123);
+    client.findOne({ id: 123, include: true }).end();
 
 Will do a `GET` request against the subresource of the specified endpoint. The
 subresource is either the `id` property of the `query` hash or if `query` is a
-string or number then this. If you do not provide a `callback` a `findOne`
-event is emitted.
+string or number then this. If you do not provide a `callback` a `request` is
+returned.
 
 #### client.persist(model[, callback])
 
     // does a POST /<endpoint>
-    client.persist({ name: 'Joe' });
+    client.persist({ name: 'Joe' }, function(error, response) {
+      // handle error and response
+    });
 
     // does a PUT /<endpoint>/<model.id>
-    client.persist({ id: 123, name: 'Joe' });
+    client.persist({ id: 123, name: 'Joe' }, function(response) {
+      // handle response
+    });
+
+    // preferred in most cases as we do not get any data in the response
+    client.persist({ id: 123, name: 'Joe' }).end();
 
 Either does a `POST` request if no `id` is present in the provided `model` or
-a `PUT` if so. If you provide no `callback` you can listen to the `persist`
-event.
+a `PUT` if so. If you provide no `callback` you will get a `request` back.
 
 #### client.destroy(model[, callback])
 
     // does a DELETE /<endpoint>/123
-    client.destroy({ id: 123 });
+    client.destroy({ id: 123 }).end();
 
     // also does a DELETE /<endpoint/123
-    client.destroy('123');
+    client.destroy('123').end();
 
 Will send a `DELETE` request to a subresource on endpoint. It will either use
 the `id` property of `model` or it will just use `model` if string or number.
-Emits a `destroy` event if no `callback` provided.
 
 ## License
 
--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
-var chai   = require('chai');
-var rester = require('../lib');
+var chai       = require('chai');
+var rester     = require('../lib');
+var superagent = require('superagent');
 
 var app = require('./server');
 
@@ -7,105 +8,131 @@ before(function() {
   app.listen(3000);
 });
 
-describe('rester(url)', function() {
+describe('Class: Rester', function() {
 
   var client;
 
-  describe('Event: "error"', function(done) {
+  describe('new Rester(options)', function() {
 
-    it('should be emitted on network error', function(done) {
-      rester.once('error', function(error) {
-        chai.expect(error).to.be.instanceof(Error);
+    it('should set "url" to "/"', function() {
+      client = new rester.Rester();
 
-        done();
-      });
-      rester('http://localhost:1234/foobar').find();
+      chai.expect(client).to.have.property('url').to.equal('/');
+    });
+
+    it('should use "url" from options', function() {
+      client = new rester.Rester({ url: '/foo' });
+
+      chai.expect(client).to.have.property('url').to.equal('/foo');
+    });
+
+    it('should set "plugins" to array', function() {
+      client = new rester.Rester();
+
+      chai.expect(client).to.have.property('plugins').to.be.an('array');
+      chai.expect(client).to.have.property('plugins').to.be.empty;
+    });
+
+    it('should use "plugins" from options', function() {
+      client = new rester.Rester({ plugins: [function() {}] });
+
+      chai.expect(client).to.have.property('plugins').to.be.an('array');
+      chai.expect(client).to.have.property('plugins').to.have.length(1);
     });
 
   });
 
-  describe('Event: "status"', function() {
+  describe('Event: "error"', function(done) {
 
-    it('should be emitted on each request', function(done) {
-      rester.once('status', function(status) {
-        chai.expect(status).to.equal(200);
+    it('should be emitted on network error', function(done) {
+      client = new rester.Rester({ url: 'http://localhost:1234/foobar' });
+
+      client.once('error', function(error) {
+        chai.expect(error).to.be.instanceof(Error);
 
         done();
       });
-      rester('http://localhost:3000/products').find();
+      client.find().end();
+    });
+
+  });
+
+  beforeEach(function() {
+    client = new rester.Rester({ url: 'http://localhost:3000/products' });
+  });
+
+  describe('Event: "response"', function() {
+
+    it('should be emitted on request with callback', function(done) {
+      client.once('response', function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+
+        done();
+      });
+      client.find(function(response) {});
+    });
+
+    it('should be emitted on request without callback', function(done) {
+      client.once('response', function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+
+        done();
+      });
+      client.find().end();
     });
 
   });
 
   describe('rester.use(plugin)', function() {
 
-    it('should register plugin on each instance', function() {
-      rester.use(function() {});
-
-      chai.expect(rester('http://localhost').plugins).to.have.length(1);
-    });
-
     it('should be called before each request', function(done) {
-      var called = 0;
-      rester.use(function(req) {
-        chai.expect(req).to.have.property('set').and.to.be.a('function');
-        chai.expect(req).to.have.property('get').and.to.be.a('function');
+      client.use(function(request) {
+        chai.expect(request).to.be.instanceof(superagent.Request);
 
-        if (!called++) done();
+        done();
       });
-
-      rester('http://localhost:3000/products').find();
+      client.find().end();
     });
 
-  });
-
-  beforeEach(function() {
-    client = rester('http://localhost:3000/products');
   });
 
   describe('client.find([query, callback])', function() {
 
-    it('should emit a "find" event', function(done) {
-      client.once('find', function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(body).to.eql(app.products);
-        chai.expect(respond).to.have.deep.property('req.path')
-          .and.to.equal('/products')
-
-        done();
-      });
-      client.find();
-    });
-
-    it('should emit a "find" event and use query', function(done) {
-      client.once('find', function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(body).to.eql(app.products);
-        chai.expect(respond).to.have.deep.property('req.path')
-          .and.to.equal('/products?limit=5')
-
-        done();
-      });
-      client.find({ limit: 5 });
-    });
-
-    it('should exec the callback', function(done) {
-      client.find(function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(body).to.eql(app.products);
-        chai.expect(respond).to.have.deep.property('req.path')
-          .and.to.equal('/products')
+    it('should return request', function(done) {
+      client.find().end(function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.body).to.eql(app.products);
+        chai.expect(response.req.path).to.eql('/products');
 
         done();
       });
     });
 
-    it('should exec the callback and use query', function(done) {
-      client.find({ limit: 5 }, function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(body).to.eql(app.products);
-        chai.expect(respond).to.have.deep.property('req.path')
-          .and.to.equal('/products?limit=5')
+    it('should execute callback', function(done) {
+      client.find(function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.body).to.eql(app.products);
+        chai.expect(response.req.path).to.eql('/products');
+
+        done();
+      });
+    });
+
+    it('should use query and return request', function(done) {
+      client.find({ limit: 5 }).end(function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.body).to.eql(app.products);
+        chai.expect(response.req.path).to.eql('/products?limit=5');
+
+        done();
+      });
+    });
+
+    it('should use query and exec callback', function(done) {
+      client.find({ limit: 5 }, function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.body).to.eql(app.products);
+        chai.expect(response.req.path).to.eql('/products?limit=5');
 
         done();
       });
@@ -115,120 +142,152 @@ describe('rester(url)', function() {
 
   describe('client.findOne(query[, callback])', function() {
 
-    it('should use id in query object', function(done) {
-      client.findOne({ id: 1 }, function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(body).to.eql(app.products[0]);
-        chai.expect(respond).to.have.deep.property('req.path')
-          .and.to.equal('/products/1');
+    it('should return request at "query"', function(done) {
+      client.findOne('1').end(function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.body).to.eql(app.products[0]);
+        chai.expect(response.req.path).to.eql('/products/1');
 
         done();
       });
     });
 
-    it('should use id in query object and query as query', function(done) {
-      client.findOne({ id: 1, include: true }, function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(body).to.eql(app.products[0]);
-        chai.expect(respond).to.have.deep.property('req.path')
-          .and.to.equal('/products/1?include=true');
+    it('should execute callback at "query"', function(done) {
+      client.findOne('1', function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.body).to.eql(app.products[0]);
+        chai.expect(response.req.path).to.eql('/products/1');
 
         done();
       });
     });
 
-    it('should use query as id', function(done) {
-      client.findOne('1', function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(body).to.eql(app.products[0]);
-        chai.expect(respond).to.have.deep.property('req.path')
-          .and.to.equal('/products/1');
+    it('should return request at "query.id"', function(done) {
+      client.findOne({ id: 1, include: true }).end(function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.body).to.eql(app.products[0]);
+        chai.expect(response.req.path).to.eql('/products/1?include=true');
 
         done();
       });
     });
 
-    it('should request with id and emit "findOne"', function(done) {
-      client.once('findOne', function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(body).to.eql(app.products[0]);
-        chai.expect(respond.req.path).to.equal('/products/1');
+    it('should execute callback at "query.id"', function(done) {
+      client.findOne({ id: 1, include: true }, function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.body).to.eql(app.products[0]);
+        chai.expect(response.req.path).to.eql('/products/1?include=true');
 
         done();
       });
-      client.findOne('1');
-    });
-
-    it('should request with query object and emit "findOne"', function(done) {
-      client.once('findOne', function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(body).to.eql(app.products[1]);
-        chai.expect(respond.req.path).to.equal('/products/2?include=true');
-
-        done();
-      });
-      client.findOne({ id: 2, include: true });
     });
 
   });
 
   describe('client.persist(model[, callback])', function() {
 
-    it('should request PUT model', function(done) {
-      app.products[1].price += 1.99;
+    it('should return PUT request', function(done) {
+      var product = app.products[1];
 
-      client.persist(app.products[1], function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(respond.req.path).to.equal('/products/2');
+      product.price += 1.22;
 
-        done();
-      });
-    });
-
-    it('should request POST model', function(done) {
-      client.persist({ name: 'car' }, function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(respond.req.path).to.equal('/products');
+      client.persist(product).end(function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.req.path).to.eql('/products/2');
+        chai.expect(response.req.method).to.eql('PUT');
 
         done();
       });
     });
 
-    it('should emit "persist" event', function(done) {
-      app.products[1].price += 1.99;
+    it('should return POST request', function(done) {
+      var product = { name: 'book', price: 9.99 };
 
-      client.once('persist', function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(respond.req.path).to.equal('/products/2');
+      client.persist(product).end(function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.req.path).to.eql('/products');
+        chai.expect(response.req.method).to.eql('POST');
 
         done();
       });
-      client.persist(app.products[1]);
+    });
+
+    it('should execute callback after PUT', function(done) {
+      var product = app.products[1];
+
+      product.price += 1.22;
+
+      client.persist(product, function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.req.path).to.eql('/products/2');
+        chai.expect(response.req.method).to.eql('PUT');
+
+        done();
+      });
+    });
+
+    it('should execute callback after POST', function(done) {
+      var product = { name: 'book', price: 9.99 };
+
+      client.persist(product, function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.req.path).to.eql('/products');
+        chai.expect(response.req.method).to.eql('POST');
+
+        done();
+      });
     });
 
   });
 
   describe('client.destroy(model[, callback])', function() {
 
-    it('should request DELETE to model.id', function(done) {
-      client.destroy(app.products[2], function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(respond.req.path).to.equal('/products/3');
+    it('should return request and use "model"', function(done) {
+      var product = app.products[0].id;
+
+      client.destroy(product).end(function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.req.path).to.eql('/products/' + product);
+        chai.expect(response.req.method).to.eql('DELETE');
 
         done();
       });
     });
 
-    it('should emit "destroy" event', function(done) {
-      var product = app.products[0];
+    it('should execute callback and use "model"', function(done) {
+      var product = app.products[0].id;
 
-      client.once('destroy', function(ok, body, respond) {
-        chai.expect(ok).to.be.true;
-        chai.expect(respond.req.path).to.equal('/products/' + product.id);
+      client.destroy(product, function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.req.path).to.eql('/products/' + product);
+        chai.expect(response.req.method).to.eql('DELETE');
 
         done();
       });
-      client.destroy(product);
+    });
+
+    it('should return request and use "model.id"', function(done) {
+      var product = app.products[0];
+
+      client.destroy(product).end(function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.req.path).to.eql('/products/' + product.id);
+        chai.expect(response.req.method).to.eql('DELETE');
+
+        done();
+      });
+    });
+
+    it('should execute callback and use "model.id"', function(done) {
+      var product = app.products[0];
+
+      client.destroy(product, function(response) {
+        chai.expect(response).to.be.instanceof(superagent.Response);
+        chai.expect(response.req.path).to.eql('/products/' + product.id);
+        chai.expect(response.req.method).to.eql('DELETE');
+
+        done();
+      });
     });
 
   });
